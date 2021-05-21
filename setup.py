0a22a04f1901ed70d72e0b9cbbb38f60faa20a5e
Tomasz Matczak <tmatczak@gmail.com>
2021-05-21 18:31:39 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='ETL_Pipeline',
+      version='1.0',
+      description='This is a simple data pipeline',
+      author='Tomasz Matczak',
+      author_email='tmatczak@gmail.com',
+      url='https://github.com/tmat84/ETL_Pipeline',
+      packages=find_packages(),
+     )
