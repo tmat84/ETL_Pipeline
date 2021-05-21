e33f24bc76d7726d191251277f79612a2807fb3a
Tomasz Matczak <tmatczak@gmail.com>
2021-05-21 17:29:36 +0200

    fix bug in the test_connection test change the folder name

--- tests/database/test_engine.py
+++ tests/database/test_engine.py
@@ -3,7 +3,7 @@ import pytest
 from database.engine import postgresEngine
 
 
-@pytest.xfail(reason="“Using TDD,is not implemented")
+@pytest.mark.xfail(reason="“Using TDD,is not implemented")
 def test_connection(params):
     """Check the connection to local database """
     with postgresEngine(**params) as db:
